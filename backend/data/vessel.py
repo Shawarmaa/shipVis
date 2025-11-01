82c33be7d333ae4e7a963cd156b8ca9477a54a76
Izhaan Salam <izhaansalam@gmail.com>
2025-11-01 21:10:46 +0000

    Now we filter by ports!

--- backend/data/vessel.py
+++ backend/data/vessel.py
@@ -6,27 +6,37 @@ import os
 
 from dotenv import load_dotenv
 load_dotenv()
+ports = ["ROTTERDAM"]
 
 
-async def connect_ais_stream():
+async def connect_ais_stream(bounding_box: list[list[float]], filter_ship_mmsi: list[str] = None,
+                             filter_message_types: list[str] = ["ShipStaticData"]):
 
     async with websockets.connect("wss://stream.aisstream.io/v0/stream") as websocket:
         subscribe_message = {"APIKey": os.getenv("AIS_API_KEY"),  # Required !
-                             "BoundingBoxes": [[[-90, -180], [90, 180]]], # Required!
-                             "FiltersShipMMSI": ["368207620", "367719770", "211476060"], # Optional!
-                             "FilterMessageTypes": ["PositionReport"]} # Optional!
+                             "BoundingBoxes": [bounding_box], # Required!
+                             "FiltersShipMMSI": filter_ship_mmsi, # Optional!
+                             "FilterMessageTypes": filter_message_types} # Optional!
 
         subscribe_message_json = json.dumps(subscribe_message)
         await websocket.send(subscribe_message_json)
-
+        output = []
         async for message_json in websocket:
             message = json.loads(message_json)
-            message_type = message["MessageType"]
 
-            if message_type == "PositionReport":
-                # the message parameter contains a key of the message type which contains the message itself
-                ais_message = message['Message']['PositionReport']
-                print(f"[{datetime.now(timezone.utc)}] ShipId: {ais_message['UserID']} Latitude: {ais_message['Latitude']} Latitude: {ais_message['Longitude']}")
+            message_type = message["MessageType"]
+            if message_type != "ShipStaticData":
+                continue
+            
+            if message["Message"]["ShipStaticData"]["Destination"].strip() not in ports:
+                continue
+            
+            print(message["Message"]["ShipStaticData"])
+            
+
+
+            
+            
 
 if __name__ == "__main__":
-    asyncio.run(asyncio.run(connect_ais_stream()))
+    asyncio.run(asyncio.run(connect_ais_stream(bounding_box=[[-90, -180], [90, 180]])))
